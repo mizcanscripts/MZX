0e5659a2816e40ffda10639188ce6a426fe5c456
Mizumaru † <akioishikawa4@gmail.com>
2026-04-23 17:15:56 +0800

    All Belong To @mizcanscripts

---
+++ api/scripts/fetch/PBR Shader v1.lua
@@ -0,0 +1,6 @@
+--[[
+	WARNING: Heads up! This script has not been verified by MZX. Use at your own risk!
+]]
+-- [[ PBR Shader V1 | by @mizuscripts aka @mizcanscripts]]
+
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/mizu-dump/PBRShader/refs/heads/main/v2"))()
