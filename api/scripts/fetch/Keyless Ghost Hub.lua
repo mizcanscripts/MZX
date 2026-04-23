59994a76735f3aa1e3821c64ad2dfffc3f9a43c7
Mizumaru † <akioishikawa4@gmail.com>
2026-04-23 16:52:18 +0800

    All Belong To @mizcanscripts

---
+++ api/scripts/fetch/Keyless Ghost Hub.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by MZX. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://codeberg.org/LEGO89/Scripts/raw/branch/main/Keyless%20GhostHub.lua"))()
