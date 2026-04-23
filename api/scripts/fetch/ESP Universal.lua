a49fef8b7db4adad381738472d5b846af9b223af
Mizumaru † <akioishikawa4@gmail.com>
2026-04-23 16:42:22 +0800

    All Belong To @mizcanscripts

---
+++ api/scripts/fetch/ESP Universal.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by MZX. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/alizeja/AliESP/refs/heads/main/main.lua"))()
