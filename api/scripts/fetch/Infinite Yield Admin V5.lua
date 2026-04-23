1021a740c242396706d094a1501ed70d10c4d8f5
Mizumaru † <akioishikawa4@gmail.com>
2026-04-23 16:46:51 +0800

    All Belong To @mizcanscripts

---
+++ api/scripts/fetch/Infinite Yield Admin V5.lua
@@ -0,0 +1,6 @@
+--[[
+	WARNING: Heads up! This script has not been verified by MZX. Use at your own risk!
+]]
+-- https://discord.gg/MY6SGnPdg2
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
